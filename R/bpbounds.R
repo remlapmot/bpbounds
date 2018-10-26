a89a83380c0d1b7281ec12ded710d35c4cde7dc1
remlapmot <remlapmot@hotmail.com>
2018-10-26 16:01:03 +0100

    edit to bpbounds helpfile and DESCRIPTION Suggests

--- R/bpbounds.R
+++ R/bpbounds.R
@@ -57,7 +57,7 @@
 #'   freq = c(74,11514,34,2385,12,9665,0,0)
 #' )
 #'
-#' tab1inddat	= uncount(tab1dat, freq)
+#' tab1inddat = uncount(tab1dat, freq)
 #' xt = xtabs( ~ x + y + z, data = tab1inddat)
 #' p = prop.table(xt, margin = 3)
 #' bpres = bpbounds(p)
