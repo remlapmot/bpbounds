a0b52271b6653e9fb6d8b350b48adb9d5dd4aade
remlapmot <remlapmot@hotmail.com>
2018-12-16 16:28:05 +0000

    Reformat bpbounds_calc_tri_z2.R

--- R/bpbounds_calc_tri_z2.R
+++ R/bpbounds_calc_tri_z2.R
@@ -1,5 +1,4 @@
-bpbounds_calc_tri_z2 <- function(p){
-
+bpbounds_calc_tri_z2 <- function(p) {
   # assuming that p is in the following order
   # notation for conditional probabilities is p(y,x|z)
   p000 = p[1]
@@ -38,18 +37,20 @@ bpbounds_calc_tri_z2 <- function(p){
   p11lower = c(p11low1, p11low2, p11low3, p11low4)
   p11upper = c(p11upp1, p11upp2, p11upp3, p11upp4)
 
-  retlist = list("p10low" = p10low,
-                 "p10upp" = p10upp,
-                 "p11low" = p11low,
-                 "p11upp" = p11upp,
-                 "p10lower" = p10lower,
-                 "p10upper" = p10upper,
-                 "p11lower" = p11lower,
-                 "p11upper" = p11upper)
+  retlist = list(
+    "p10low" = p10low,
+    "p10upp" = p10upp,
+    "p11low" = p11low,
+    "p11upp" = p11upp,
+    "p10lower" = p10lower,
+    "p10upper" = p10upper,
+    "p11lower" = p11lower,
+    "p11upper" = p11upper
+  )
 
   # bounds on causal risk ratio
-  rrlow = p11low/p10upp
-  rrupp = p11upp/p10low
+  rrlow = p11low / p10upp
+  rrupp = p11upp / p10low
   retlist = append(retlist, list("crrlb" = rrlow,
                                  "crrub" = rrupp))
 
@@ -60,8 +61,9 @@ bpbounds_calc_tri_z2 <- function(p){
   m4 = p111 - p110 >= 0
   mlow = p000 - p001 - p011 - p101
   mupp = p000 + p010 + p110 - p011
-  monoinequality = (m1 == TRUE & m2 == TRUE & m3 == TRUE & m4 == TRUE)
-  if  (monoinequality) {
+  monoinequality = (m1 == TRUE &
+                      m2 == TRUE & m3 == TRUE & m4 == TRUE)
+  if (monoinequality) {
     retlist = append(retlist, list("monobplb" = mlow,
                                    "monobpub" = mupp))
 
@@ -70,16 +72,22 @@ bpbounds_calc_tri_z2 <- function(p){
     monop10upp = 1 - p000
     monop11low = p111
     monop11upp = 1 - p011
-    retlist = append(retlist, list("monop10lb" = monop10low,
-                                   "monop10ub" = monop10upp,
-                                   "monop11lb" = monop11low,
-                                   "monop11ub" = monop11upp))
+    retlist = append(
+      retlist,
+      list(
+        "monop10lb" = monop10low,
+        "monop10ub" = monop10upp,
+        "monop11lb" = monop11low,
+        "monop11ub" = monop11upp
+      )
+    )
 
     # bounds on causal risk ratio assuming monotonicity
-    monocrrlow = monop11low/monop10upp
-    monocrrupp = monop11upp/monop10low
-    retlist = append(retlist, list("monocrrlb" = monocrrlow,
-                                   "monocrrub" = monocrrupp))
+    monocrrlow = monop11low / monop10upp
+    monocrrupp = monop11upp / monop10low
+    retlist = append(retlist,
+                     list("monocrrlb" = monocrrlow,
+                          "monocrrub" = monocrrupp))
   }
   retlist = append(retlist, list("monoinequality" = monoinequality))
   return(retlist)
